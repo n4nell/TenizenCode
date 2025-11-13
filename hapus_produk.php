f97b043c88ed05c42a93bad036199b3900b8c962
n4nell <khairinnaila08@gmail.com>
2025-11-13 13:54:17 +0700

    second commit

---
+++ hapus_produk.php
@@ -0,0 +1,15 @@
+<?php
+require_once('connect.php');
+
+if (isset($_POST['idproduk'])) {
+    $idproduk = $_POST['idproduk'];
+    $query = mysqli_query($con, "DELETE FROM produk WHERE idproduk='$idproduk'");
+
+    if ($query) {
+        echo "Data produk berhasil dihapus";
+    } else {
+        echo "Gagal menghapus data produk";
+    }
+} else {
+    echo "Parameter idproduk tidak ada";
+}
